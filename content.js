55e0d2294f18a4f07e4627c017419ab09f06bb17
niC00L <nika.blanarova@gmail.com>
2024-04-08 20:44:22 +0200

    Handlebars template + update content for 2024

---
+++ content.js
@@ -0,0 +1,76 @@
+var context = {
+    section1: {
+        "id": "uvod",
+        "menu": "Úvod",
+        "heading": "českomoravský<br>pohár rallye<br>2024",
+        "text": "Jakožto zapálení fanoušci motorsportu se chceme zúčastnit celé sezóny ČMPR a děláme pro to maximum. Společné volné chvíle trávíme laděním důležitých komponentů nebo detailů na autě, protože víme, že na detailech záleží.",
+    },
+    section2: {
+        "id": "posadka",
+        "menu": "Posádka",
+        "driver-name": "Miroslav Žák",
+        "driver-text": "Sny se mají plnit, proto se rozhodl jít do rallye naplno. Jeho cit pro techniku a praktická zručnost tvoří pevné jádro týmu, k čemuž přispívá i jeho neoblomný životní optimismus.Dobré předpoklady pro pozici řidiče získal mimo jiné i v autokrosu.Ve volném čase se věnuje motorkám a dceři.",
+        "navigator-name": "Barbora Vykopalová",
+        "navigator-text": "Již od mala byla raději v garáži, panenky měnila za angličáky, radostí se podílí na údržbě nejen závodního vozidla.Rychlé jízdy se nebojí, naopak si ji užívá po boku zkušeného řidiče.Občas se projíždí na motorce,         občas tříbí orientační smysly v lese po boku se svým psem."
+    },
+    section3: {
+        "id": "sezona",
+        "menu": "Sezóna",
+        "heading": "Sezóna 2024",
+        "text": "Letos se budeme účastnit veškerých podniků pod Českomoravským pohárem Rallye (ČMPR). Pro tuto chvíli je známo 5termínů konání závodů.Budeme se svým vozem startovat v kategorii A2, tedy vozy do obsahu motoru 1600 ccm.",
+        "races": [
+            {
+                "date": "26.-27.4.2023",
+                "name": "Rallye Králiky",
+                "link": "https://www.rallyekraliky.cz/"
+            },
+            {
+                "date": "24.-25.5. 2024",
+                "name": "Podorlická rally",
+                "link": "http://www.podorlickarally.cz/"
+            },
+            {
+                "date": "21.-22.6. 2024",
+                "name": "Radouňská rallye",
+                "link": "http://www.radounmotorsport.cz/"
+            },
+            {
+                "date": "19.-20.7. 2024",
+                "name": "Rallye Železné hory",
+                "link": "https://www.rallyezeleznehory.cz/"
+            },
+            {
+                "date": "10.-11.8. 2024",
+                "name": "Slovácká Rallye Kyjov",
+                "link": ""
+            },
+            {
+                "date": "13.-14.9. 2024",
+                "name": "Rallye Kroměříž",
+                "link":
+                    "https://www.ewrc-results.com/final/72062-rallye-kromeriz-2021/?fbclid=IwAR2EFCaY84Blth0gVxdvOy11FI19u7FHU0rn1c-CmRKuJDkgoOOAtFcy1UA"
+            },
+            {
+                "date": "11.-12.10. 2024",
+                "name": "Rallye Hořovice",
+                "link": "http://askhorovice.cz/"
+            }
+        ]
+    },
+    section4: {
+        "id": "finance",
+        "menu": "Finance",
+        "heading": "POTŘEBNÉ FINANCE",
+        "vozidlo": "117 000 KČ",
+        "logistika": "78 000 KČ",
+        "pneumatiky": "52 000 KČ",
+        "startovne": "84 000 KČ"
+    },
+    section5: {
+        "id": "kontakt",
+        "menu": "Kontakt",
+        "heading": "Kontaktujte nás",
+        "email": "mb-motorsport@gmail.com",
+        "phone": "+420 773 100 205"
+    }
+};
